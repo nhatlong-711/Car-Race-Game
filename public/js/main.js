dc38cf5805b28cbbc523c4aef3b3198fce091a36
Nhat Long <longhoang072004@gmail.com>
2024-01-09 16:45:54 +0700

    upload source code

---
+++ public/js/main.js
@@ -0,0 +1,67 @@
+
+var run1 = 0
+var run2 = 0
+var move = document.getElementById('car1')
+function moveFirstCar() {
+    run1 += 5
+    console.log('move first car')
+    var move = document.getElementById('car1')
+    move.style.marginLeft = run1 + 'px'
+    var en1 = document.getElementById('en1')
+    en1.style.width = run1*(200/1030) + 'px' 
+}
+function moveSecondCar() {
+    run2 += 5
+    console.log('move second car')
+    var move = document.getElementById('car2')
+    move.style.marginLeft = run2 + 'px'
+    var en2 = document.getElementById('en2')
+    en2.style.width = run2*(200/1030) + 'px' 
+}
+const myTimeout = setTimeout(start, 30000)
+function start() {
+    var car1 = document.getElementById("car1")
+    var car2 = document.getElementById("car2")
+    var red = document.getElementById("red");
+    var yellow = document.getElementById("yellow");
+    var green = document.getElementById("green");
+    var go = document.getElementById('go');
+    car1.classList.add('car_runing')
+    car2.classList.add('car_runing')
+    setTimeout(function () {
+        red.style.backgroundColor = 'red';
+    }, 1000)
+    setTimeout(function () {
+        yellow.style.backgroundColor = 'yellow';
+    }, 2000)
+    setTimeout(function () {
+        green.style.backgroundColor = 'green';
+        go.innerHTML = 'Chạy!!!';
+        document.addEventListener("keydown", (event) => {
+            switch (event.key) {
+                case 'ArrowRight':
+                    moveFirstCar()
+                    break;
+                case 'd':
+                    moveSecondCar()
+                    break;
+            }
+        });
+        checkwinner()
+    }, 3000)
+    function checkwinner(){
+        var check = setInterval(function () {
+            if (parseInt(car1.style.marginLeft) >= 1030) {
+                alert("Xe vàng thắng!");
+                clearInterval(check);
+            }
+            if (parseInt(car2.style.marginLeft) >= 1030) {
+                alert("Xe đỏ thắng!");
+                clearInterval(check);
+            }
+        }, 100);
+    }
+}
+function again(){
+    location.reload()
+}
